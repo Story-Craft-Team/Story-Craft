33f4e6baa2b45d9ebde4896ef736fc1a11064266
CrazyAnry <ivasenkoandrej76@gmail.com>
2025-06-03 20:30:59 +0700

    Added Refresh token controller

--- backend/src/modules/deffault/helpers/services/user-auth.helpers.service.ts
+++ backend/src/modules/deffault/helpers/services/user-auth.helpers.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../../prisma/prisma.service";
+import { JwtService } from "@nestjs/jwt";
+import { AuthService } from "../../auth/services/auth.service";
 
 @Injectable()
 export class UserAuthHelperService {
-    constructor(private prisma:PrismaService) {}
+    constructor(private prisma:PrismaService,
+                private readonly jwtService: JwtService,
+                private readonly authService: AuthService,
+    ) {}
     async revokeToken(token: string){
         try{
             await this.prisma.revokedToken.create({
                 data:{
-                token
+                    token
                 }
             })
             return { message: "User has been logged out"}
@@ -24,6 +29,23 @@ export class UserAuthHelperService {
                 where: { token },
             });
             return revokedToken !== null;
+        }
+        catch(error){
+            throw error
+        }
+    }
+
+    async refreshAccessToken(refreshToken: string){
+        try{
+            const data = this.jwtService.decode(refreshToken)
+            console.log(data)
+            const accessToken = await this.authService.generateToken(data)
+            return {
+                tokens: {
+                    accessToken: accessToken,
+                    refreshToken,
+                }
+            }
         }
         catch(error){
             throw error
